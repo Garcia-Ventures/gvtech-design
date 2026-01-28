530f97bda7f2e9b5409523510caeaded3f34e5df
Eric N. Garcia <eng618@garciaericn.com>
2026-01-28 03:07:07 -0500

    refactor: update ESLint configuration to include React and accessibility plugins, and adjust ignored files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,10 +5,13 @@ import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
 import storybook from 'eslint-plugin-storybook';
+import react from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
   {
-    ignores: ['dist', 'node_modules', '.yarn', 'coverage', 'eslint.config.mjs'],
+    ignores: ['dist', 'node_modules', '.yarn', 'storybook-static', 'coverage', 'eslint.config.mjs'],
   },
   js.configs.recommended,
   {
@@ -44,8 +47,31 @@ export default [
   },
   {
     files: ['**/*.js', '**/*.jsx', '**/*.mjs', '**/*.cjs'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+    },
+    plugins: {
+      react,
+      'jsx-a11y': jsxA11y,
+      'react-hooks': reactHooks,
+    },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
   {
