c081063ae893b0bd95450b3025b8c81cdcc40b94
Eric N. Garcia <eng618@garciaericn.com>
2026-02-06 03:00:08 -0500

    feat: add `direction` prop to `ResizablePanelGroup` and map it to `orientation`

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -4,8 +4,18 @@ import { Group, Panel, Separator } from 'react-resizable-panels';
 
 import { cn } from '@/lib/utils';
 
-const ResizablePanelGroup = ({ className, ...props }: React.ComponentProps<typeof Group>) => (
-  <Group className={cn('flex h-full w-full data-[panel-group-direction=vertical]:flex-col', className)} {...props} />
+const ResizablePanelGroup = ({
+  className,
+  direction,
+  ...props
+}: React.ComponentProps<typeof Group> & {
+  direction: 'horizontal' | 'vertical';
+}) => (
+  <Group
+    orientation={direction}
+    className={cn('flex h-full w-full data-[panel-group-direction=vertical]:flex-col', className)}
+    {...props}
+  />
 );
 
 const ResizablePanel = Panel;
